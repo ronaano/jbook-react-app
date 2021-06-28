de4b1a073a1c39bf9f19ff0bb798365261dceff0
Ronan <binnyronan@gmail.com>
2021-06-28 21:42:17 +1200

    Setup additional options for editor

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -6,7 +6,14 @@ const CodeEditor = () => {
   return (
     <MonacoEditor
       options={{
-        wordWrap: 'on'
+        wordWrap: 'on',
+        minimap: { enabled: false },
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        scrollBeyondLastLine: false,
+        automaticLayout: true
       }}
       theme='dark'
       language='javascript'
