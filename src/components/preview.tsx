830e67a21bfdd5aef0418dfe1c8ba483f2411d0b
Ronan <binnyronan@gmail.com>
2021-06-30 23:01:43 +1200

    clean up

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -1,29 +1,28 @@
-import { useRef, useEffect } from 'react';
 import './preview.css';
+import { useRef, useEffect } from 'react';
 
 interface PreviewProps {
   code: string;
 }
 
 const html = `
-  <html>
-    <head>
-    </head>
-    <body>
-      <div id="root"></div>
-      <script>
-        window.addEventListener('message', (event) => {
-          try {
-          eval(event.data);
-          } catch (err) {
-            const root = document.querySelector('#root');
-            root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>'; 
-            console.err(err);
-          }
-        }, false);
-      </script>
-    </body>
-  </html>
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            try {
+              eval(event.data);
+            } catch (err) {
+              const root = document.querySelector('#root');
+              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+              console.error(err);
+            }
+          }, false);
+        </script>
+      </body>
+    </html>
   `;
 
 const Preview: React.FC<PreviewProps> = ({ code }) => {
@@ -35,12 +34,14 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
   }, [code]);
 
   return (
-    <iframe
-      ref={iframe}
-      sandbox='allow-scripts'
-      title='preview'
-      srcDoc={html}
-    />
+    <div className='preview-wrapper'>
+      <iframe
+        title='preview'
+        ref={iframe}
+        sandbox='allow-scripts'
+        srcDoc={html}
+      />
+    </div>
   );
 };
 
