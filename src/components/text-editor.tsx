6e3acf2508f32b712bee21fc918dbbd3d8087754
Ronan <binnyronan@gmail.com>
2021-07-01 23:20:28 +1200

    Setup conditional toggling for editor, fixed css class conflicts

---
+++ src/components/text-editor.tsx
@@ -0,0 +1,43 @@
+import { useState, useEffect, useRef } from 'react';
+import MDEditor from '@uiw/react-md-editor';
+
+const TextEditor: React.FC = () => {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const [editing, setEditing] = useState(false);
+  const [value, setValue] = useState('# Header');
+
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if (
+        ref.current &&
+        event.target &&
+        ref.current.contains(event.target as Node)
+      ) {
+        return;
+      }
+
+      setEditing(false);
+    };
+    document.addEventListener('click', listener, { capture: true });
+
+    return () => {
+      document.removeEventListener('click', listener, { capture: true });
+    };
+  }, []);
+
+  if (editing) {
+    return (
+      <div ref={ref}>
+        <MDEditor value={value} onChange={(v) => setValue(v || '')} />
+      </div>
+    );
+  }
+
+  return (
+    <div onClick={() => setEditing(true)}>
+      <MDEditor.Markdown source={value} />
+    </div>
+  );
+};
+
+export default TextEditor;
