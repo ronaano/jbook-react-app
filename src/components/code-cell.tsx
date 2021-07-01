4e0019dbb5f2abb8b244c30e626f5e6e2c7d3246
Ronan <binnyronan@gmail.com>
2021-07-01 14:16:20 +1200

    Handling async and bundling errors

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -6,12 +6,14 @@ import Resizable from './resizable';
 
 const CodeCell = () => {
   const [input, setInput] = useState('');
+  const [err, setErr] = useState('');
   const [code, setCode] = useState('');
 
   useEffect(() => {
     const timer = setTimeout(async () => {
       const output = await bundle(input);
-      setCode(output);
+      setCode(output.code);
+      setErr(output.err);
     }, 1000);
     return () => {
       clearTimeout(timer);
@@ -27,7 +29,7 @@ const CodeCell = () => {
             onChange={(value) => setInput(value)}
           />
         </Resizable>
-        <Preview code={code} />
+        <Preview code={code} bundlingStatus={err} />
       </div>
     </Resizable>
   );
--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -3,6 +3,7 @@ import { useRef, useEffect } from 'react';
 
 interface PreviewProps {
   code: string;
+  bundlingStatus: string;
 }
 
 const html = `
@@ -11,13 +12,20 @@ const html = `
       <body>
         <div id="root"></div>
         <script>
+        const handleError = (err) => {
+          const root = document.querySelector('#root');
+          root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+          console.error(err);
+        }
+          window.addEventListener('error', (event) => {
+            event.preventDefault();
+            handleError(event.error);
+          })
           window.addEventListener('message', (event) => {
             try {
               eval(event.data);
             } catch (err) {
-              const root = document.querySelector('#root');
-              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
-              console.error(err);
+                handleError(err);
             }
           }, false);
         </script>
@@ -25,7 +33,7 @@ const html = `
     </html>
   `;
 
-const Preview: React.FC<PreviewProps> = ({ code }) => {
+const Preview: React.FC<PreviewProps> = ({ code, bundlingStatus }) => {
   const iframe = useRef<any>();
 
   useEffect(() => {
@@ -43,6 +51,9 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
         sandbox='allow-scripts'
         srcDoc={html}
       />
+      {bundlingStatus && (
+        <div className='preview-bundleStatus'>{bundlingStatus}</div>
+      )}
     </div>
   );
 };
