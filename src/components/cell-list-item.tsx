71b8a62c62df5be2d5a91dc5cf31725bad347520
Ronan <binnyronan@gmail.com>
2021-07-05 22:41:22 +1200

    Addng action bar component

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -1,13 +1,19 @@
 import { CellProps } from '../interfaces/cellProps';
 import CodeCell from './code-cell';
 import TextEditor from './text-editor';
+import ActionBar from './action-bar';
 
 const CellListItem: React.FC<CellProps> = ({ cell }) => {
   let child: JSX.Element;
   cell.type === 'code'
     ? (child = <CodeCell cell={cell} />)
     : (child = <TextEditor cell={cell} />);
-  return <div>{child}</div>;
+  return (
+    <div>
+      <ActionBar id={cell.id} />
+      {child}
+    </div>
+  );
 };
 
 export default CellListItem;
--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -42,7 +42,7 @@ const TextEditor: React.FC<CellProps> = ({ cell }) => {
   return (
     <div className='text-editor card' onClick={() => setEditing(true)}>
       <div className='card-content'>
-        <MDEditor.Markdown source={cell.content} />
+        <MDEditor.Markdown source={cell.content || 'Click to edit'} />
       </div>
     </div>
   );
