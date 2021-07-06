74badc770cc82a2fa4570b77aa19f39a95927718
Ronan <binnyronan@gmail.com>
2021-07-06 23:29:54 +1200

    Added initial progress bar to application

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -5,6 +5,7 @@ import Resizable from './resizable';
 import { Cell } from '../state';
 import { useActions } from '../hooks/useActions';
 import { useTypedSelector } from '../hooks/used-typed-selector';
+import './code-cell.css';
 interface CodeCellProps {
   cell: Cell;
 }
@@ -41,7 +42,15 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
             onChange={(value) => updateCell(cell.id, value)}
           />
         </Resizable>
-        {bundle && <Preview code={bundle.code} bundlingStatus={bundle.err} />}
+        {!bundle || bundle.loading ? (
+          <div className='progress-cover'>
+            <progress className='progress is-small is-primary' max='100'>
+              Loading
+            </progress>
+          </div>
+        ) : (
+          <Preview code={bundle.code} bundlingStatus={bundle.err} />
+        )}
       </div>
     </Resizable>
   );
