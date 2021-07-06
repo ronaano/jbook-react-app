fbd9a689ea592770c0d4c2c717ac98186abe43bd
Ronan <binnyronan@gmail.com>
2021-07-06 23:11:01 +1200

    Added eager bundling

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -12,13 +12,18 @@ interface CodeCellProps {
 const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const { updateCell, createBundle } = useActions();
   const bundle = useTypedSelector((state) => state.bundles[cell.id]);
+
   useEffect(() => {
+    if (!bundle) {
+      createBundle(cell.id, cell.content);
+    }
     const timer = setTimeout(async () => {
       createBundle(cell.id, cell.content);
     }, 1000);
     return () => {
       clearTimeout(timer);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [cell.content, cell.id, createBundle]);
 
   return (
