dd38e92fc7b7aa6e83e5697839d721a9a268685d
Ronan <binnyronan@gmail.com>
2021-07-07 14:08:39 +1200

    Added selective show code cell logic

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -17,27 +17,32 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     const { data, order } = state.cells;
     const orderedCells = order.map((id) => data[id]);
 
-    const cumulativeCodeCells = [
-      `
-      const show = (value) => {
+    const showFunc = `
+    var show = (value) => {
 
-        import _React from 'react';
-        import _ReactDOM from 'react-dom';
-        const root = document.querySelector('#root');
-        if(typeof value === 'object') {
-          if(value.$$typeof && value.props) {
-            ReactDOM.render(value, root);
-          } else {
-            root.innerHTML = JSON.stringify(value);
-        }
-        } else{
-          root.innerHTML = value;
-        }
+      import _React from 'react';
+      import _ReactDOM from 'react-dom';
+      const root = document.querySelector('#root');
+      if(typeof value === 'object') {
+        if(value.$$typeof && value.props) {
+          _ReactDOM.render(value, root);
+        } else {
+          root.innerHTML = JSON.stringify(value);
       }
-      `
-    ];
+      } else{
+        root.innerHTML = value;
+      }
+    }
+    `;
+    const showFuncNoop = 'var show = () => {}';
+    const cumulativeCodeCells = [];
     for (let c of orderedCells) {
       if (c.type === 'code') {
+        if (c.id === cell.id) {
+          cumulativeCodeCells.push(showFunc);
+        } else {
+          cumulativeCodeCells.push(showFuncNoop);
+        }
         cumulativeCodeCells.push(c.content);
       }
       if (c.id === cell.id) {
